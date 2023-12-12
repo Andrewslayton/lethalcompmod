96d8212fe431fbebde3f752ce79eaba789855d69
Andrew Slayton <96811667+Andrewslayton@users.noreply.github.com>
2023-12-11 19:52:34 -0500

    Delete BrutalCompany directory

--- BrutalCompany/NotSoBrutalCompany/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NotSoBrutalCompany
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "NotSoBrutalCompany";
-        public const string PLUGIN_NAME = "NotSoBrutalCompany";
-        public const string PLUGIN_VERSION = "1.4.0";
-    }
-}
--- BrutalCompany/obj/Release/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Project1
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BrutalCompany";
-        public const string PLUGIN_NAME = "BrutalCompany";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
